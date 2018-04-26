1b4a89f7066a8f6a5b268c12b77c9930a99453db
dlfdnd0725@gmail.com <dlfdnd0725@gmail.com>
2018-04-26 15:28:17 +0900

    init : project start

---
+++ src/Main.scala
@@ -0,0 +1,12 @@
+/**
+  *
+  * Created By iw.jhun
+  * On 2018-04-26
+  *
+  */
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    print(SomeObject.formatString(9))
+  }
+}
---
+++ src/SomeObject.scala
@@ -0,0 +1,14 @@
+/**
+  *
+  * Created By iw.jhun
+  * On 2018-04-26
+  *
+  */
+object SomeObject {
+
+  def formatString(x : Int)  : String= {
+    val msg = "Your Input Integer.. => %d"
+    msg.format(x)
+  }
+
+}
